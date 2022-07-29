9729eaaae7aaa259e3ab360585ba13f4d6703ff1
Nisha Majhi <84006994+NishaMajhi@users.noreply.github.com>
2022-07-29 21:54:54 +0530

    Create DataTypeAndVariable.js

---
+++ JAVASCRIPT/DataTypeAndVariable.js
@@ -0,0 +1,40 @@
+// variable
+
+//syntax : var var_name;
+//syntax : var var_name = value;
+
+
+//Data types
+
+// 1) Number
+var num1 = 40;
+var num2 = 40.5;
+console.log(num1+num2);
+
+// 2) string
+var str1 = "this is a string";
+var str2 = 'this is also a string';
+console.log(str1);
+console.log(str2);
+//console.log(str1+str2);
+
+// 3) Objects - key:value pair
+var subjects = {
+     DAA:301,
+     DBMS:501,
+     OS:502,
+     CN:603
+}
+console.log(subjects);
+
+// 4) Boolean
+var a = true;
+var b = false;
+console.log(a,b);
+
+// 5) NULL or UnIdentified
+var x = null;
+console.log(x);
+
+var y; //we didn't assign any value 
+console.log(y);
