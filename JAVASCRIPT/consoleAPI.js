2c15cc0b6a5a6357d497bafbd85686c106a6e69b
Nisha Majhi <84006994+NishaMajhi@users.noreply.github.com>
2022-07-29 21:55:28 +0530

    Add files via upload

---
+++ JAVASCRIPT/consoleAPI.js
@@ -0,0 +1,11 @@
+
+//ways to print anything
+document.write("this is document.write - hello World");
+alert("hello world");
+console.log("hello world");
+
+//console APT's
+console.log("hello world", 10 + 4, "hello again");
+console.warn("this is a warning");
+console.error("this is a error");
+
